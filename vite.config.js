14fbd56568d00b54dd35c40dcfddd60cff676ae0
quartzquadrant8 <jabr0n_13x@proton.me>
2025-07-09 16:19:23 -0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // <-- ADD THIS LINE
 
 export default defineConfig({
-  plugins: [
-    react(), // <-- ADD THIS LINE
-  ],
   server: {
     proxy: {
       '/api': {
